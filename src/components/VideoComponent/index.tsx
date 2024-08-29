0c823f4b50a93cbb920d7e17449b0449b39af5b8
fadilfahrudin <fadilfahrudin32@gmail.com>
2024-08-30 05:03:16 +0700

    bug fix

--- src/components/VideoComponent/index.tsx
+++ src/components/VideoComponent/index.tsx
@@ -1,10 +1,36 @@
-const VideoComponent = ({src}: {src: string}) => {
+import { useEffect, useRef } from "react";
+
+const VideoComponent = ({ src }: { src: string }) => {
+    const ref = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const video = ref.current;
+
+        if (video) {
+            // Set muted to ensure autoplay on iOS
+            video.muted = true;
+
+            // Try to play the video and handle any possible promise rejection
+            const playPromise = video.play();
+
+            if (playPromise !== undefined) {
+                playPromise
+                    .then(() => {
+                        console.log("Video is playing.");
+                    })
+                    .catch(error => {
+                        console.log("Failed to play the video.", error);
+                    });
+            }
+        }
+    }, []);
+
     return (
-        <video width="320" height="240" loop autoPlay muted>
+        <video ref={ref} width="320" height="240" loop autoPlay muted>
             <source src={src} type="video/mp4" />
             Your browser does not support the video tag.
         </video>
-    )
-}
+    );
+};
 
-export default VideoComponent
+export default VideoComponent;
