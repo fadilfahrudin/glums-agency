682da9b833bb4a06d35b2098a57cb2b0df4824a3
Fadil Fahruddin <fadilfahrudin32@gmail.com>
2024-10-31 02:05:16 +0700

    update service ui

--- src/page/service/index.tsx
+++ src/page/service/index.tsx
@@ -1,8 +1,8 @@
-import { motion } from 'framer-motion'
+import { motion, MotionValue, useScroll, useTransform } from 'framer-motion'
 import "./services.scss";
 import HeaderWording from '../../components/header-wording'
 import Gap from '../../components/gap';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useGetServicesQuery } from '../../utils/redux/services/serviceApi';
 interface Props {
     id: number,
@@ -10,11 +10,16 @@ interface Props {
     service_banner_path: string,
     service_name: string,
     excerpt: string
-    sub_excerpt: string
+}
+
+
+function useParallax(value: MotionValue<number>, distance: number) {
+    return useTransform(value, [0, 1], [-distance, distance])
 }
 const Services = () => {
     const target = useRef<HTMLElement | null>(null)
     const { data, isSuccess } = useGetServicesQuery({ keywords: "" })
+
     return (
         <motion.main id='services' >
             <HeaderWording
@@ -25,10 +30,10 @@ const Services = () => {
                 desc='We believe that every brand has a unique story to tell, and we are here to help you amplify that story through innovative and effective digital strategies.'
             />
 
-            <motion.div ref={target as React.RefObject<HTMLDivElement>} className="container">
+            <motion.div ref={target as React.RefObject<HTMLDivElement>} className="container mainServices">
                 <div id='wrapper-main' className="wrapper-main" >
                     {
-                        isSuccess && data?.data.map((data: Props, i: number) => <CardImage key={data.id} id={data.id} service_banner_path={data.service_banner_path} service_name={data.service_name} excerpt={data.excerpt} sub_excerpt={data.sub_excerpt} i={i} />)
+                        isSuccess && data?.data.map((data: Props, i: number) => <CardImage key={data.id} id={data.id} service_banner_path={data.service_banner_path} service_name={data.service_name} excerpt={data.excerpt} i={i} />)
                     }
                 </div>
             </motion.div>
@@ -39,17 +44,25 @@ const Services = () => {
 
 
 
-const CardImage = ({ id, service_banner_path, service_name, excerpt, sub_excerpt, i }: Props) => {
-    const [hoveredId, setHoveredId] = useState<number | null>(null)
+const CardImage = ({ id, service_banner_path, service_name, excerpt, i }: Props) => {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({ target: ref, offset: ['-100vh', '100vh'] })
+    const y = useParallax(scrollYProgress, 1000)
 
     return (
-        <motion.a href={`service/detail/${id}`} onMouseEnter={() => setHoveredId(id)} onMouseLeave={() => setHoveredId(null)} whileHover={{ flexBasis: '549px', flexShrink: 0 }} transition={{ duration: 0.5 }} className={`img-item ${hoveredId == id ? 'active' : ''}`} >
-            <img src={service_banner_path} alt="img" loading='lazy' width={549} height={793} />
-            <div className="info">
+        <motion.a href={`service/detail/${id}`} className={`img-item`} >
+            <motion.div style={{ y }}  className="info">
                 <motion.div className='number'>0{i + 1}</motion.div>
-                <motion.div initial={window.innerWidth > 1080 ? { opacity: 0 } : { opacity: 1 }} animate={window.innerWidth > 1080 ? { opacity: hoveredId == id ? 1 : 0 } : ''} transition={hoveredId == id ? { duration: 0.3 } : { duration: 0.3 }} className='role'>{service_name}</motion.div>
-                <motion.div initial={window.innerWidth > 1080 ? { opacity: 0 } : { opacity: 1 }} animate={window.innerWidth > 1080 ? { opacity: hoveredId == id ? 1 : 0 } : ''} transition={hoveredId == id ? { duration: 0.3 } : { duration: 0.3 }} className="title">{excerpt}</motion.div>
-                <motion.div initial={window.innerWidth > 1080 ? { opacity: 0 } : { opacity: 1 }} animate={window.innerWidth > 1080 ? { opacity: hoveredId == id ? 1 : 0 } : ''} transition={hoveredId == id ? { duration: 0.3 } : { duration: 0.3 }} className="desc">{sub_excerpt}</motion.div>
+                <motion.div className="excerpt">{excerpt}</motion.div>
+                <motion.div className='detail_service'>
+                    <motion.div className='service_wrapper'>
+                        <motion.div className='service_name'>{service_name}</motion.div>
+                        <motion.div className="sub_service">Content Creation, Website Traffic Analysis, Social Media Management</motion.div>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+            <div ref={ref} className='imgWrapper'>
+                <img src={service_banner_path} alt="img" loading='lazy' width={549} height={793} />
             </div>
         </motion.a>
     )
